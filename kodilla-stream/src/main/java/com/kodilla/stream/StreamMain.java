63b46a7ab23214dab5faf01bd7730fed7fb456e3
Adam Pietka <adam.pietk@gmail.com>
2021-01-30 15:28:25 +0100

    7.2. Czym jest Stream - correction for immutable class

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import  com.kodilla.stream.beautifier.PoemBeautifier;
+import  com.kodilla.stream.person.People;
 
 
 public class StreamMain {
@@ -39,6 +40,14 @@ public class StreamMain {
         System.out.println("Using Stream to generate even numbers from 1 to 20");
         NumbersGenerator.generateEven(20);
 
+        System.out.println("\n****************************");
+        People.getList().stream()
+//                .map(s -> s.toUpperCase())
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') +2) + ".")
+                .filter(s -> s.substring(0,1).equals("M"))
+                .forEach(System.out::println);
 
         /*
         expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -1,10 +1,10 @@
 package com.kodilla.stream.immutable;
 
-public class ForumUser {
-    private String username;
-    private String realName;
+public final class ForumUser {
+    private final String username;
+    private final String realName;
 
-    public ForumUser(final String username, String realName) {
+    public ForumUser(final String username, final String realName) {
         this.username = username;
         this.realName = realName;
     }
