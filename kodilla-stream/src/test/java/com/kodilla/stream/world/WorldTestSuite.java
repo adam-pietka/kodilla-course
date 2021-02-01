af4c6f82d9a2092094ceddfbce153f954bd991ca
Adam Pietka <adam.pietk@gmail.com>
2021-02-01 17:33:22 +0100

    Poznanie flatMap i reduce/correction of test ver.2

--- kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -12,10 +12,12 @@ public class WorldTestSuite {
         World world = new World();
         Continent continentAsia = new Continent("Asia");
         Continent continentEurope = new Continent("Europe");
+        Continent continentSouthAmerica = new Continent("South America");
 
         //when
         world.addContinent(continentAsia);
         world.addContinent(continentEurope);
+        world.addContinent(continentSouthAmerica);
 
         continentAsia.addCountryToContinent(new Country ( new BigDecimal("565"), "Japan"));
         continentAsia.addCountryToContinent(new Country ( new BigDecimal("435"), "China"));
@@ -23,15 +25,13 @@ public class WorldTestSuite {
         continentEurope.addCountryToContinent(new Country(new BigDecimal("101"), "Poland"));
         continentEurope.addCountryToContinent(new Country(new BigDecimal("301"), "Denmark"));
 
+        continentSouthAmerica.addCountryToContinent(new Country(new BigDecimal("1001"), "Brazil"));
+        continentSouthAmerica.addCountryToContinent(new Country(new BigDecimal("301"), "Argentina"));
+
         //then
         world.quantityPeopleInWord();
-        int i =   world.getContinentList().size();
-        System.out.println(i);
-
-        Assertions.assertEquals(5, world.quantityPeopleInWord());
-
-
-
+        BigDecimal correctResult = new BigDecimal("2704");
+        Assertions.assertEquals(correctResult, world.quantityPeopleInWord());
     }
 
 }
