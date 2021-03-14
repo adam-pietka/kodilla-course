2323a79b8e56bdaf442eefec7b42852248ca432f
Adam Pietka <adam.pietk@gmail.com>
2021-03-14 15:06:07 +0100

    13.1 Zadanie: funkcyjna iteracja po tytułach

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challanges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challanges/MovieStore.java
@@ -1,13 +1,12 @@
 package com.kodilla.good.patterns.challanges;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class MovieStore {
 
     public Map<String, List<String>> getMovies() {
+
         List<String> ironManTranslations = new ArrayList<>();
         ironManTranslations.add("Żelazny Człowiek");
         ironManTranslations.add("Iron Man");
@@ -25,6 +24,19 @@ class MovieStore {
         booksTitlesWithTranslations.put("AV", avengersTranslations);
         booksTitlesWithTranslations.put("FL", flashTranslations);
 
+
+        booksTitlesWithTranslations.entrySet().stream()
+            .flatMap(x-> x.getValue().stream())
+                .collect(Collectors.toSet())
+                .forEach(x-> System.out.print(x + "! "));
+
         return booksTitlesWithTranslations;
+    }
+
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+        movieStore.getMovies();
+
     }
 }
